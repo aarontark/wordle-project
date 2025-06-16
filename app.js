ccf325349239a70f25c6f6841c8cf5a5e5d74760
aarontark <tarkington.aaron@gmail.com>
2025-06-16 15:57:56 -0500

    responsive test 8

--- app.js
+++ app.js
@@ -163,3 +163,8 @@ restartBtn.addEventListener('click', () => {
 })
 
 loadWords();
+
+// code for mobile
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
